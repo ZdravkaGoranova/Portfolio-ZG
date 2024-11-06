5a8f0e1969b1eb59adbc299957dbfe1d54fa182c
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-11-06 19:49:17 +0200

    adding animation into Home Component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,6 +3,15 @@ import './Home.css';
 const Home = () => {
   return (
     <>
+      <div className="bars-animation">
+        <div className="bar" style={{ '--i': 6 }}></div>
+        <div className="bar" style={{ '--i': 5 }}></div>
+        <div className="bar" style={{ '--i': 4 }}></div>
+        <div className="bar" style={{ '--i': 3 }}></div>
+        <div className="bar" style={{ '--i': 2 }}></div>
+        <div className="bar" style={{ '--i': 1 }}></div>
+      </div>
+
       <section className="home">
         <div className="home-info">
           <h1>Zdravka Goranova</h1>
@@ -28,6 +37,14 @@ const Home = () => {
               <a href="#">
                 <i className="bx bxl-gitlab"></i>
               </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="home-img">
+          <div className="img-box">
+            <div className="img-item">
+              <img src="./image-zdr.jpg" alt="" />
             </div>
           </div>
         </div>
