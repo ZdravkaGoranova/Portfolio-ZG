fc0d1b92a7ebb4956cf39a8cb2f722896b9d34ea
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-11-06 18:06:16 +0200

    create Home Component

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import './Home.css';
+
+const Home = () => {
+  return (
+    <>
+      <section className="home">
+        <div className="home-info"></div>
+        <h1>Zdravka Goranova</h1>
+        
+      </section>
+    </>
+  );
+};
+
+export default Home;
