38ed6b8cf4af2b828b04ec0d53ec2c90bb2e41ac
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-11-06 18:43:46 +0200

    adding home info and custome style css

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,9 +4,33 @@ const Home = () => {
   return (
     <>
       <section className="home">
-        <div className="home-info"></div>
-        <h1>Zdravka Goranova</h1>
-        
+        <div className="home-info">
+          <h1>Zdravka Goranova</h1>
+          <h2>I&apos;m a Frontend Developer</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+            explicabo, reprehenderit laboriosam, officia vero delectus dolores
+            numquam doloremque debitis eius quas praesentium enim accusamus!
+            Cumque illo dolorum maxime porro perferendis.
+          </p>
+          <div className="btn-sci">
+            <a href="" className="btn">
+              Download CV
+            </a>
+            <div className="sci">
+              <a href="#">
+                <i className="bx bxl-linkedin"></i>
+              </a>
+              <a href="#">
+                <i className="bx bxl-github"></i>
+              </a>
+
+              <a href="#">
+                <i className="bx bxl-gitlab"></i>
+              </a>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
