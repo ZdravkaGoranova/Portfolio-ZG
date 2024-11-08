c207c25af11d283ab26c5dfb4fa17622bd04219c
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-11-08 18:20:22 +0200

    update App.jsx and css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import Header from './Components/Header/Header.jsx';
 import Home from './Components/Home/Home.jsx';
 
 function App() {
+  
+  // let navbar = document.querySelector('.navbar');
+  // let menu = document.querySelector('#menu-icon');
+  // menu.onClick = () => {
+  //   menu.classList.toggle('bx-x');
+  //   navbar.classList.toggle('open');
+  // };
   return (
     <>
       <Header />
